39602e37342c6f67155061989290ca0f8d8a0d77
Anton <antos.horbach@gmail.com>
2021-06-20 13:36:17 +0300

    fixed project template

---
+++ sandbox/cmake/Cache.cmake
@@ -0,0 +1,8 @@
+if (ENABLE_CACHE)
+    find_program(CCACHE ccache)
+    if (CCACHE)
+        set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+    else()
+        message(WARNING "cache is enabled but was not found.")
+    endif ()
+endif ()
---
+++ sandbox/src/SOME/@ENTITY_NAME.cpp
@@ -0,0 +1,3 @@
+
+#include "SOME/config.h"
+                
